5f89490d95b68f3e3e28bd562e62067b4630f6be
guillermosifu <sifuentes1201@gmail.com>
2022-09-01 21:06:27 -0500

    call

--- src/AppEjemplo.js
+++ src/AppEjemplo.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState,use } from "react";
 //el primer paso para definir nuestro componenente es el NOMBRE
 //EL nombre debe iniciar con MAYUCULA 
 // ahora el componente es basicamnente una funcion por ende podemos crearlo usansi 
@@ -17,19 +17,26 @@ import { useState } from "react";
 const [count,setCount] = useState(0)
 
 const sumar =()=>{
-   setCount(count + 10);
+   setCount(count + 1);
 }
 const restar =()=>{
     setCount(count -1);
 }
 
+const parametros= (a,b)=>{
+    setCount(a + b);
+};
+
 return(
 <div>
 <h1>ESTE ES MI COMPONENTE {count}</h1>
 {/* dentro de cada etiqueta se llama a la vriable entre llaves  */}
-<h2>holla desde componente{count}</h2>
+<h2>holla desde componente</h2>
 <button onClick={sumar}>sumar </button>
-<button onClick={restar}>restar</button>
+<button onClick={restar}>restar {count}</button>
+{/* si queremo usas parentesis al momento de llamar un funcion debemos primero ejecutarlos como un callback */}
+{/* ademas es la forma en el caso nuestro reciba el paramaetro */}
+<button  onClick={()=>parametros (100,20)}>calcular</button>
 </div>
 )
 
