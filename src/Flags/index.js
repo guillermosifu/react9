8cd15e5cc06d7b73ada19126088b638cace4dc6c
guillermosifu <sifuentes1201@gmail.com>
2022-09-14 23:01:04 -0500

    flags3

--- src/Flags/index.js
+++ src/Flags/index.js
@@ -8,100 +8,119 @@ import {
   MenuItem,
   Card,
   CardMedia,
-  CardContent,
+  CardContent,CircularProgress,
 } from "@mui/material";
 import { useEffect, useState } from "react";
 import { getDataFromPokemon } from "../services";
 
 const Flags = () => {
-  const [countries, setCountries] = useState([]);
-
-  const [region, setRegion] = useState("");
-
-  const fetchCountries = async () => {
-    const response = await getDataFromPokemon(
-      "https://restcountries.com/v3.1/all"
-    );
-
-    setCountries(response);
-  };
-
-  const handleRegion = async (e) => {
-    setRegion(e.target.value);
-    // vamos a evala si el valor es igual all entonces ejecutas la funcion
-    //fetchCountries
-    if (e.target.value === "all") {
-      fetchCountries();
-      return;
-    }
-    // primero debemos limpiar para llebar con nueva informacion
-    setCountries([]);
-    const response = await getDataFromPokemon(
-      `https://restcountries.eu/rest/v2/region/${e.target.value}`
-    );
-
-    setCountries(response);
-  };
-  //vamos a crea una funcion el cuala se encaragra de buscar los paises
-  const handleSearchCountry = (e) => {
-    //esto es buena practixa decirle que busque por 3 a 4 letras la peticion
-    const countryName = e.target.value;
-    if (countryName.length === 0) {
-      fetchCountries();
-    }
-    if (countryName.length > 3) {
-      const filterCountries = countries.filter((country) =>
-        country.name.official.toUpperCase().includes(countryName.toUpperCase())
+    const [countries, setCountries] = useState([]);
+  
+    const [region, setRegion] = useState("");
+  
+    const fetchCountries = async () => {
+      const response = await getDataFromPokemon(
+        "https://restcountries.com/v3.1/all"
       );
-      setCountries(filterCountries);
-    }
-  };
-
-  useEffect(() => {
-    fetchCountries();
-  }, []);
-
-  return (
-    <Container>
-      <Grid container spacing={3} mt={5}>
-        <Grid item md={6} sm={4}>
-          <TextField
-            onChange={handleSearchCountry}
-            label="busca tu bandera"
-            fullWidth
-          />
-        </Grid>
-        <Grid item md={6} sm={4}>
-          <FormControl fullWidth>
-            <InputLabel>busca tu region</InputLabel>
-            <Select label="busca tu bandera" onChange={handleRegion}>
-              <MenuItem value="all">Todas las regiones</MenuItem>
-
-              <MenuItem value="Africa">Africa</MenuItem>
-              <MenuItem value="Americas">Americas</MenuItem>
-              <MenuItem value="Asia">Asia</MenuItem>
-              <MenuItem value="Europe">Erurope</MenuItem>
-              <MenuItem value="Oceania">Oceania</MenuItem>
-            </Select>
-          </FormControl>
-        </Grid>
-        {countries.length >
-          0  (
+      setCountries(response);
+    };
+  
+    const handleRegion = async (e) => {
+      setRegion(e.target.value);
+      // vamos a evaluar si el valor es igual a all entonces ejecutsa la funcion
+      // fetchCountries
+      if (e.target.value === "all") {
+        fetchCountries();
+        return;
+      }
+  
+      // primero debemos limpiar para poder llenarlo con la nueva informacion
+      setCountries([]);
+      const response = await getDataFromPokemon(
+        `https://restcountries.com/v3.1/region/${e.target.value}`
+      );
+      setCountries(response);
+    };
+  
+    // vamos a crear una funcion la cual se encargue de buscar los paises
+    const handleSearchCountry = (e) => {
+      // Es una buena practica decirle que inicie a contar cuando tengamos mas de 3 letras
+      const countryName = e.target.value;
+  
+      if (countryName.length === 0) {
+        fetchCountries();
+      }
+  
+      if (countryName.length > 3) {
+        // aca debemos iniciar la busqueda
+        // para poder hacer la busqueda debeo transformar todo el text a UPPERCASE or LOWECASE
+        const filterCountries = countries.filter((country) =>
+          country.name.official.toUpperCase().includes(countryName.toUpperCase())
+        );
+        setCountries(filterCountries);
+      }
+    };
+  
+    useEffect(() => {
+      fetchCountries();
+    }, []);
+  
+    return (
+      <Container>
+        <Grid container spacing={3} mt={5}>
+          <Grid item md={6}>
+            <TextField
+              onChange={handleSearchCountry}
+              label="Busca tu Pais..."
+              fullWidth
+            />
+          </Grid>
+          <Grid item md={6}>
+            <FormControl fullWidth>
+              <InputLabel>Filter by Region</InputLabel>
+              <Select
+                label="Filtra por Region"
+                value={region}
+                onChange={handleRegion}
+              >
+                <MenuItem value="all">Todas las regiones</MenuItem>
+                <MenuItem value="Africa">Africa</MenuItem>
+                <MenuItem value="Americas">Americas</MenuItem>
+                <MenuItem value="Asia">Asia</MenuItem>
+                <MenuItem value="Europe">Europe</MenuItem>
+                <MenuItem value="Oceania">Oceania</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          {countries.length > 0 ? (
             countries.map((country) => (
-              <Grid item md ={3} sx={12}>
-                <Card>
-                  <CardMedia component="img" image={country.flags.svg} />
-
-                  <CardContent>
-                    <h4>{country.name.official}</h4>
-                  </CardContent>
-                </Card>
+              <Grid item md={3} xs={12}>
+                
+                  <Card>
+                    <CardMedia
+                      component="img"
+                      height={200}
+                      image={country.flags.svg}
+                    />
+                    <CardContent>
+                      <h4>{country.name.common}</h4>
+                      <p>Population: {country.population}</p>
+                      <p>Region: {country.region}</p>
+                      <p>Capital: {country.capital}</p>
+                    </CardContent>
+                  </Card>
+              
               </Grid>
             ))
+          ) : (
+            <div className="center loading">
+              <CircularProgress />
+              <h4>Cargando...</h4>
+            </div>
           )}
-      </Grid>
-    </Container>
-  );
-};
+        </Grid>
+      </Container>
+    );
+  };
 
 export default Flags;
